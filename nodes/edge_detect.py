1c12c21ac78ae9b858f89c7ed7a1efe80495318e
sneaky-potato <rajivkamal.im421@gmail.com>
2021-11-02 19:57:23 +0530

    implemented bridge

--- nodes/edge_detect.py
+++ nodes/edge_detect.py
@@ -1,15 +1,33 @@
 #!/usr/bin/env python
 import rospy
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+import cv2
 
-# known pump geometry
-#  - units are pixels (of half-size image)
-PUMP_DIAMETER = 360
-PISTON_DIAMETER = 90
-PISTON_COUNT = 7
+def showImage(img):
+    cv2.imshow('image', img)
+    cv2.waitKey(1)
+
+def process_image(msg):
+    try:
+       pass
+    except Exception as err:
+        print(err)
+    bridge = CvBridge()
+    # converting Image object to cv2 image matrix
+    orig = bridge.imgmsg_to_cv2(msg, "bgr8")
+    orig = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
+    drawImg = orig
+    # show results
+    showImage(drawImg)
+
 
 def start_node():
     rospy.init_node('edge_detect')
     rospy.loginfo('edge_detect node started')
+    # Sending image to process_image function
+    rospy.Subscriber("image", Image, process_image)
+    rospy.spin()
 
 if __name__ == '__main__':
     try:
