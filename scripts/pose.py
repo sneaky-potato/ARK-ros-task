a152f64f5d1df3bac23dc6806e5cd494ec6f2a8c
sneaky-potato <rajivkamal.im421@gmail.com>
2021-11-26 00:29:10 +0530

    pose calculator done

---
+++ scripts/pose.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import rospy
+from tf.transformations import quaternion_matrix
+import std_msgs
+import re
+import sys
+import numpy as np
+
+def start_node(filepath):
+    rospy.init_node('pose_pub')
+    rospy.loginfo('pose_pub node started')
+    pub = rospy.Publisher('pose', std_msgs.msg.Float32MultiArray, queue_size=10)
+
+    if (filepath[-1] != '/'): filepath += '/'
+    projections = np.zeros((10, 3, 4))
+    print(projections.shape)
+    for i in range(10):
+        f = open(filepath + str(i) + '_2.txt', "r")
+        vector = f.read()
+        vector = re.sub('<.*?>', '', vector)
+        dictionary = eval(vector)
+
+        rmat = quaternion_matrix([dictionary['orientation']['w_val'], dictionary['orientation']['x_val'], dictionary['orientation']['y_val'], dictionary['orientation']['z_val']])
+        R = rmat[:3,:3]
+        #print(R)
+        tvec = np.matrix([[dictionary['position']['x_val']], [dictionary['position']['y_val']], [dictionary['position']['z_val']]])
+        extrinsic = np.hstack([R.T, -R.T.dot(tvec)])
+
+        f_x = 256 // 2
+        f_y = f_x
+        c_x = 256 // 2
+        c_y = 144 // 2
+
+        intrinsic = np.matrix([[f_x, 0, c_x], [0, f_y, c_y], [0, 0, 1]])
+
+        projection = intrinsic.dot(extrinsic)
+        print(projection)
+        projections[i, :, :] = projection
+    while not rospy.is_shutdown():
+        data_to_send = std_msgs.msg.Float32MultiArray()  # the data to be sent, initialise the array
+        
+        data_to_send.data = projections.flatten()
+        pub.publish(data_to_send)
+        rospy.Rate(1.0).sleep()  # 1 Hz
+
+
+if __name__ == '__main__':
+    try:
+        start_node( rospy.myargv(argv=sys.argv)[1])
+    except rospy.ROSInterruptException or IndexError:
+        print("Usage: roscore [package_name] [node] [filepath]")
+        pass
