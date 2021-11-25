617127dc2249ea7585f1a9108ac999638fbb7355
sneaky-potato <rajivkamal.im421@gmail.com>
2021-11-26 01:01:07 +0530

    helper function added

--- scripts/main.py
+++ scripts/main.py
@@ -4,6 +4,9 @@ import std_msgs
 import numpy as np
 import cv2
 
+def get_distance(point1, point2):
+    return np.sqrt((point1[0]-point2[0])**2 + (point1[1] - point2[1])**2 + (point1[2]-point2[2])**2)
+
 class Triangulate:
     def __init__(self):
         self.pose = None
@@ -26,14 +29,17 @@ class Triangulate:
     
     def get_length(self):
         if(self.pose is not None and self.corners is not None): 
-            
+
             # Data recieved
             for i in range(0, 10, 2):
-                upper_lower_point = cv2.triangulatePoints(self.pose[i], self.pose[i+1], self.corners[i], self.corners[i+1])
-                upper_lower_point[:, 0] = upper_lower_point[:, 0] / upper_lower_point[3, 0]
-                upper_lower_point[:, 1] = upper_lower_point[:, 1] / upper_lower_point[3, 1]
-                diff = (upper_lower_point[:, 0] - upper_lower_point[:, 1])
-                print('length=', np.sqrt(diff[0]**2 + diff[1]**2 + diff[2]**2))
+                # upper_lower_point = cv2.triangulatePoints(self.pose[i], self.pose[i+1], self.corners[i], self.corners[i+1])
+                # upper_lower_point[:, 0] = upper_lower_point[:, 0] / upper_lower_point[3, 0]
+                # upper_lower_point[:, 1] = upper_lower_point[:, 1] / upper_lower_point[3, 1]
+                # diff = (upper_lower_point[:, 0] - upper_lower_point[:, 1])
+                print('length=', 0)
+
+    def get_centroid(self):
+        pass
 
 
 def start_node():
@@ -43,7 +49,10 @@ def start_node():
     solution_triangulate = Triangulate()
     rospy.Subscriber("corners", std_msgs.msg.Float32MultiArray, solution_triangulate.corners_callback)
     rospy.Subscriber("pose", std_msgs.msg.Float32MultiArray, solution_triangulate.pose_callback)
+
+    # Invoke the solution
     solution_triangulate.get_length()
+    solution_triangulate.get_centroid()
     rospy.spin()
 
 
