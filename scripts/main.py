e1c60791f2e38f9687a18216192636c968a0e397
sneaky-potato <rajivkamal.im421@gmail.com>
2021-11-26 00:55:58 +0530

    unnecessary code removed

--- scripts/main.py
+++ scripts/main.py
@@ -8,33 +8,26 @@ class Triangulate:
     def __init__(self):
         self.pose = None
         self.corners = None
-        #self.ts = message_filters.TimeSynchronizer([self.pose, self.corners], 10)
-        #self.ts.registerCallback(self.get_length)
 
     def pose_callback(self, msg):
         # "Store" message received.
         projections = np.array(msg.data)
         projections.shape = (10, 3, 4)
         self.pose = projections
-        #print("------- pose ------")
-        #print("pose data=", self.pose)
-        #print("corner data=", self.corners)
 
     def corners_callback(self, msg):
         # "Store" the message received.
         cornerList = np.array(msg.data)
         cornerList.shape = (10, 2, 2)
         self.corners = cornerList
-        #print("------- corner ------")
-        #print("pose data=", self.pose)
-        #print("corner data=", self.corners)
+
+        # Invoke the function
         self.get_length()
     
     def get_length(self):
-        #print("pose=", self.pose)
-        #print("corner=", self.corners)
         if(self.pose is not None and self.corners is not None): 
-            #print("goddit")
+            
+            # Data recieved
             for i in range(0, 10, 2):
                 upper_lower_point = cv2.triangulatePoints(self.pose[i], self.pose[i+1], self.corners[i], self.corners[i+1])
                 upper_lower_point[:, 0] = upper_lower_point[:, 0] / upper_lower_point[3, 0]
